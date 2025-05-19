72a18e89df048914a6799c75221bb2fc6f2c1db3
Kemi-Elizabeth <97071326+Kemi-Elizabeth@users.noreply.github.com>
2025-05-19 12:19:22 +0100

    Changed host, port and debug status. Also changed OPEN API key.

--- app.py
+++ app.py
@@ -59,4 +59,6 @@ def index():
     return render_template('index.html', tweet=tweet)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=8888)
+
+
